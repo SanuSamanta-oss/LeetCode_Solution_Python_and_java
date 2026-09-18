213e70c5722484f0dea401108449374ff3a133c4
SanuSamanta-oss <changeislaw777@gmail.com>
2026-09-18 22:03:07 +0530

    Reverse INteger problem solved

---
+++ Reverse_integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int reverse_1= 0;
+        while(x != 0){
+            
+            int digits = x % 10;
+            if (reverse_1 > Integer.MAX_VALUE / 10 ||
+                reverse_1 < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            reverse_1 = reverse_1 * 10 + digits;
+            x = x / 10;
+        }
+        return reverse_1;
+    }
+}
